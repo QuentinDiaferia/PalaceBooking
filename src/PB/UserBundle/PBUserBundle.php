e6da68f770336337138bf4640609c67fdfc822fb
QuentinDiaferia <q.diaferia@gmail.com>
2017-04-06 18:29:37 +0200

    FOSUserBundle

--- src/PB/UserBundle/PBUserBundle.php
+++ src/PB/UserBundle/PBUserBundle.php
@@ -4,6 +4,8 @@ namespace PB\UserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class PBUserBundle extends Bundle
-{
+class PBUserBundle extends Bundle {
+	public function getParent() {
+		return 'FOSUserBundle';
+	}
 }
